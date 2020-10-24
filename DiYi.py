05ff0980c206bdf1325f5a3af7139767b75ca58e
cm <1296279026@qq.com>
2020-10-24 16:53:26 +0800

    第一次上传python代码

---
+++ DiYi.py
@@ -0,0 +1,35 @@
+#第一个代码
+
+#print('hello world!')
+
+#print(6+6)
+
+#导入别人写的乌龟
+import turtle
+#创建一个乌龟
+my_turtle=turtle.Turtle()
+#让乌龟有个乌龟的形状
+my_turtle.shape("turtle")
+#乌龟向前走100步
+my_turtle.forward( 300 )
+#让乌龟右转90度
+my_turtle.right(90)
+#乌龟再向前走100步
+my_turtle.forward( 300 )
+#让乌龟右转90度
+my_turtle.right(90)
+#乌龟再向前走100步
+my_turtle.forward( 300 )
+#让乌龟右转90度
+my_turtle.right(90)
+#乌龟再向前走100步
+my_turtle.forward( 300 )
+turtle.mainloop()
+name="Chenming"
+print(name)
+print(name[5])
+print(len(name))
+print(name[0:8])
+print((name+"\n")*5)
+
+
