456083a078ab7b201b9c12f3c96e3f859f9eceed
cm <1296279026@qq.com>
2020-10-24 17:57:21 +0800

    第3次测试

---
+++ DiEr.py
@@ -0,0 +1,66 @@
+
+
+
+
+# lang=['C','C++','C#','Java','Python']
+# print(lang)
+#
+# print(len(lang))#打印数组的长度
+#
+# print(lang[3])#打印数组的第四个数据
+#
+# lang.append('Golang')#给数组里添加一个新元素
+#
+# print(lang)
+#
+# lang.remove('C++')#把数组里的C++删除
+#
+# print(lang)
+#
+# print(lang.count('C'))#数一下里面有几个“C”
+
+
+
+
+# MyName=['cm','cmx','cjx']
+# NowName=('cm','cmx','cjx')
+# print(type(MyName))
+# print(type(NowName))
+# print(len(MyName))
+# print(len(NowName))
+# print(dir(MyName))
+# print('--------------------------------------------')
+# print(dir(NowName))
+
+
+
+# age=int(input('请输入你的年龄'))
+#
+# if age>18:
+#     print('你可以上网')
+# elif age<18:
+#     print('你不可以上网')
+# else:
+#     print('你18岁，可以上网！')
+
+
+for i in range(201):
+    if i>=20:
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
